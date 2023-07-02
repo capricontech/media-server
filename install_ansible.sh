d2604db09be9638695365b468ec5d90739597394
Romit Karmakar <romitkarmakar@gmail.com>
2023-07-02 19:34:56 +0000

    Added golang orchestrator

--- install_ansible.sh
+++ install_ansible.sh
@@ -2,3 +2,4 @@ sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt update -y
 sudo apt install ansible -y
 python3 -m pip install ansible-lint
+sudo apt install netcat-openbsd -y
---
+++ server/tcp_server.go
@@ -0,0 +1,49 @@
+package server
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strconv"
+	"strings"
+)
+
+func handleConnection(c net.Conn) {
+	fmt.Printf("Serving %s\n", c.RemoteAddr().String())
+	for {
+		netData, err := bufio.NewReader(c).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		temp := strings.TrimSpace(string(netData))
+		if temp == "STOP" {
+			break
+		}
+
+		result := strconv.Itoa(20) + "\n"
+		c.Write([]byte(string(result)))
+	}
+	c.Close()
+}
+
+func StartServer() {
+
+	PORT := ":8000"
+	l, err := net.Listen("tcp4", PORT)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer l.Close()
+
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		go handleConnection(c)
+	}
+}
